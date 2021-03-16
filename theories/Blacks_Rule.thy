538496ebbbce69e9ab7e49435f8893b0271475ce
Michael Kirsten <kirsten@kit.edu>
2021-03-16 17:34:24 +0100

    The handbook actually calls this rule the pairwise majority rule

--- theories/Blacks_Rule.thy
+++ theories/Blacks_Rule.thy
@@ -7,22 +7,22 @@
 section \<open>Black's Rule\<close>
 
 theory Blacks_Rule
-  imports Condorcet_Rule
+  imports Pairwise_Majority_Rule
           Borda_Rule
 begin
 
 text
 \<open>This is Black's voting rule. It is composed of a function that determines
-the Condorcet winner and the Borda rule. Whenever there exists no Condorcet
-winner, it elects the choice made by the Borda rule, otherwise the Condorcet
-winner is elected.\<close>
+the Condorcet winner, i.e., the Pairwise Majority rule, and the Borda rule.
+Whenever there exists no Condorcet winner, it elects the choice made by the
+Borda rule, otherwise the Condorcet winner is elected.\<close>
 
 subsection \<open>Definition\<close>
 
 fun blacks_rule :: "'a Electoral_Module" where
-  "blacks_rule A p = (condorcet_rule \<triangleright> borda_rule) A p"
+  "blacks_rule A p = (pairwise_majority_rule \<triangleright> borda_rule) A p"
 
 fun blacks_rule_code :: "'a Electoral_Module" where
-  "blacks_rule_code A p = (condorcet_rule_code \<triangleright> borda_rule_code) A p"
+  "blacks_rule_code A p = (pairwise_majority_rule_code \<triangleright> borda_rule_code) A p"
 
 end
--- theories/Pairwise_Majority_Rule.thy
+++ theories/Pairwise_Majority_Rule.thy
@@ -1,39 +1,39 @@
-(*  File:       Condorcet_Rule.thy
+(*  File:       Pairwise_Majority_Rule.thy
     Copyright   2021  Karlsruhe Institute of Technology (KIT)
 *)
 \<^marker>\<open>creator "Stephan Bohr, Karlsruhe Institute of Technology (KIT)"\<close>
 \<^marker>\<open>contributor "Michael Kirsten, Karlsruhe Institute of Technology (KIT)"\<close>
 
-section \<open>Condorcet Rule\<close>
+section \<open>Pairwise Majority Rule\<close>
 
-theory Condorcet_Rule
+theory Pairwise_Majority_Rule
   imports "Compositional_Structures/Basic_Modules/Condorcet_Module"
           "Compositional_Structures/Defer_One_Loop_Composition"
 begin
 
 text
-\<open>This is a voting rule that implements the Condorcet criterion, i.e., it elects
-the Condorcet winner if it exists, otherwise a tie remains between all
-alternatives.\<close>
+\<open>This is the pairwise majority rule, a voting rule that implements the
+Condorcet criterion, i.e., it elects the Condorcet winner if it exists,
+otherwise a tie remains between all alternatives.\<close>
 
 subsection \<open>Definition\<close>
 
-fun condorcet_rule :: "'a Electoral_Module" where
-  "condorcet_rule A p = elector condorcet A p"
+fun pairwise_majority_rule :: "'a Electoral_Module" where
+  "pairwise_majority_rule A p = elector condorcet A p"
 
-fun condorcet_rule_code :: "'a Electoral_Module" where
-  "condorcet_rule_code A p = elector condorcet_code A p"
+fun pairwise_majority_rule_code :: "'a Electoral_Module" where
+  "pairwise_majority_rule_code A p = elector condorcet_code A p"
 
 fun condorcet' :: "'a Electoral_Module" where
 "condorcet' A p =
   ((min_eliminator condorcet_score) \<circlearrowleft>\<^sub>\<exists>\<^sub>!\<^sub>d) A p"
 
-fun condorcet_rule' :: "'a Electoral_Module" where
-"condorcet_rule' A p = iterelect condorcet' A p"
+fun pairwise_majority_rule' :: "'a Electoral_Module" where
+"pairwise_majority_rule' A p = iterelect condorcet' A p"
 
 subsection \<open>Condorcet Consistency Property\<close>
 
-theorem condorcet_condorcet: "condorcet_consistency condorcet_rule"
+theorem condorcet_condorcet: "condorcet_consistency pairwise_majority_rule"
 proof -
   have
     "condorcet_consistency (elector condorcet)"
@@ -41,7 +41,7 @@ proof -
     by metis
   thus ?thesis
     using condorcet_consistency2 electoral_module_def
-          condorcet_rule.simps
+          pairwise_majority_rule.simps
     by metis
 qed
 
