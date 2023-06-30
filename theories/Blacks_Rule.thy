376764e9e8821c636d0214b79e2b11c74553471a
Michael Kirsten <kirsten@kit.edu>
2023-06-30 22:11:50 +0200

    Minor renaming

--- theories/Blacks_Rule.thy
+++ theories/Blacks_Rule.thy
@@ -22,18 +22,18 @@ subsection \<open>Definition\<close>
 
 declare seq_comp_alt_eq[simp]
 
-fun blacks :: "'a Electoral_Module" where
-  "blacks A p = (condorcet \<triangleright> borda) A p"
+fun black :: "'a Electoral_Module" where
+  "black A p = (condorcet \<triangleright> borda) A p"
 
 fun blacks_rule :: "'a Electoral_Module" where
-  "blacks_rule A p = elector blacks A p"
+  "blacks_rule A p = elector black A p"
 
 declare seq_comp_alt_eq[simp del]
 
 subsection \<open>Soundness\<close>
 
-theorem blacks_sound: "electoral_module blacks"
-  unfolding blacks.simps
+theorem blacks_sound: "electoral_module black"
+  unfolding black.simps
   using seq_comp_sound condorcet_sound borda_sound
   by metis
 
@@ -44,14 +44,14 @@ theorem blacks_rule_sound: "electoral_module blacks_rule"
 
 subsection \<open>Condorcet Consistency Property\<close>
 
-theorem blacks_is_dcc: "defer_condorcet_consistency blacks"
-  unfolding blacks.simps
+theorem black_is_dcc: "defer_condorcet_consistency black"
+  unfolding black.simps
   using condorcet_is_dcc borda_mod_non_blocking borda_mod_non_electing seq_comp_dcc
   by metis
 
-theorem blacks_condorcet: "condorcet_consistency blacks_rule"
+theorem black_condorcet: "condorcet_consistency blacks_rule"
   unfolding blacks_rule.simps
-  using blacks_is_dcc dcc_imp_cc_elector
+  using black_is_dcc dcc_imp_cc_elector
   by metis
 
 end
