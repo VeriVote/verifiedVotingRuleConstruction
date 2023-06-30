5ce19143a2e2ef820f422aa3d76846d690549fce
Michael Kirsten <kirsten@kit.edu>
2023-06-30 22:08:36 +0200

    Black's rule is Condorcet consistent

--- theories/Blacks_Rule.thy
+++ theories/Blacks_Rule.thy
@@ -22,16 +22,36 @@ subsection \<open>Definition\<close>
 
 declare seq_comp_alt_eq[simp]
 
+fun blacks :: "'a Electoral_Module" where
+  "blacks A p = (condorcet \<triangleright> borda) A p"
+
 fun blacks_rule :: "'a Electoral_Module" where
-  "blacks_rule A p = elector (condorcet \<triangleright> borda) A p"
+  "blacks_rule A p = elector blacks A p"
 
 declare seq_comp_alt_eq[simp del]
 
 subsection \<open>Soundness\<close>
 
+theorem blacks_sound: "electoral_module blacks"
+  unfolding blacks.simps
+  using seq_comp_sound condorcet_sound borda_sound
+  by metis
+
 theorem blacks_rule_sound: "electoral_module blacks_rule"
   unfolding blacks_rule.simps
-  using elector_sound seq_comp_sound condorcet_sound borda_sound
+  using blacks_sound elector_sound
+  by metis
+
+subsection \<open>Condorcet Consistency Property\<close>
+
+theorem blacks_is_dcc: "defer_condorcet_consistency blacks"
+  unfolding blacks.simps
+  using condorcet_is_dcc borda_mod_non_blocking borda_mod_non_electing seq_comp_dcc
+  by metis
+
+theorem blacks_condorcet: "condorcet_consistency blacks_rule"
+  unfolding blacks_rule.simps
+  using blacks_is_dcc dcc_imp_cc_elector
   by metis
 
 end
