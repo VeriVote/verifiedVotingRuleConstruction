dc4a481e52e452f5599128d878021cf9f90ce2de
Michael Kirsten <kirsten@kit.edu>
2023-07-05 05:45:45 +0200

    Fixed definition of 'neq_ord'

--- theories/Compositional_Structures/Basic_Modules/Component_Types/Distance.thy
+++ theories/Compositional_Structures/Basic_Modules/Component_Types/Distance.thy
@@ -7,12 +7,12 @@ section \<open>Distance\<close>
 
 theory Distance
   imports "HOL-Library.Extended_Real"
-          "Social_Choice_Types/Profile"
           "HOL-Combinatorics.List_Permutation"
+          "Social_Choice_Types/Profile"
 begin
 
 text \<open>
-  A general distance on a set X is a mapping d : X x X to R union {+infty} such
+  A general distance on a set X is a mapping d : X x X to R union {+\<infinity>} such
   that for every x, y, z in X the following four conditions are satisfied:
   - d(x, y) >= 0 (nonnegativity);
   - d(x, y) = 0 if and only if x = y (identity of indiscernibles);
@@ -30,7 +30,7 @@ type_synonym 'a Distance = "'a \<Rightarrow> 'a \<Rightarrow> ereal"
 definition distance :: "'a set \<Rightarrow> 'a Distance \<Rightarrow> bool" where
   "distance S d \<equiv> \<forall> x y. (x \<in> S \<and> y \<in> S) \<longrightarrow> (d x x = 0 \<and> 0 \<le> d x y)"
 
-subsection \<open>TODO\<close>
+subsection \<open>Conditions\<close>
 
 definition symmetric :: "'a set \<Rightarrow> 'a Distance \<Rightarrow> bool" where
   "symmetric S d \<equiv> \<forall> x y. (x \<in> S \<and> y \<in> S) \<longrightarrow> d x y = d y x"
@@ -41,25 +41,17 @@ definition triangle_ineq :: "'a set \<Rightarrow> 'a Distance \<Rightarrow> bool
 definition eq_if_zero :: "'a set \<Rightarrow> 'a Distance \<Rightarrow> bool" where
   "eq_if_zero S d \<equiv> \<forall> x y. (x \<in> S \<and> y \<in> S) \<longrightarrow> d x y = 0 \<longrightarrow> x = y"
 
-abbreviation vote_distance_property :: "('a Vote set \<Rightarrow> 'a Vote Distance \<Rightarrow> bool)
-                                     \<Rightarrow> 'a Vote Distance \<Rightarrow> bool" where
-  "vote_distance_property p d \<equiv> p {(A, v). linear_order_on A v \<and> finite A} d"
+abbreviation vote_distance_property :: "('a Vote set \<Rightarrow> 'a Vote Distance \<Rightarrow> bool) \<Rightarrow>
+                                          'a Vote Distance \<Rightarrow> bool" where
+  "vote_distance_property \<pi> d \<equiv> \<pi> {(A, p). linear_order_on A p \<and> finite A} d"
 
-abbreviation election_distance_property :: "('a Election set
-                                           \<Rightarrow> 'a Election Distance \<Rightarrow> bool)
-                                           \<Rightarrow> 'a Election Distance
-                                           \<Rightarrow> bool" where
-  "election_distance_property p d \<equiv> p {(A, p). finite_profile A p} d"
+abbreviation election_distance_property :: "('a Election set \<Rightarrow> 'a Election Distance \<Rightarrow> bool) \<Rightarrow>
+                                              'a Election Distance \<Rightarrow> bool" where
+  "election_distance_property \<pi> d \<equiv> \<pi> {(A, p). finite_profile A p} d"
 
-definition el_distance_anonymity :: "'a Election Distance \<Rightarrow> bool" where
-  "el_distance_anonymity d \<equiv>
-    \<forall> C B pi p q. (\<forall> n. (pi n) permutes {..< n}) \<longrightarrow>
-    d (C, p) (B, q) = d (C, permute_list (pi (length p)) p) (B, permute_list (pi (length q)) q)"
+subsection \<open>Auxiliary Lemmas\<close>
 
-definition standard :: "'a Election Distance \<Rightarrow> bool" where
- "standard d \<equiv> \<forall> C B p q. length p \<noteq> length q \<or> C \<noteq> B \<longrightarrow> d (C, p) (B, q) = \<infinity>"
-
-lemma sum_mon:
+lemma sum_monotone:
   fixes
     A :: "'a set" and
     f :: "'a \<Rightarrow> int" and
@@ -67,20 +59,48 @@ lemma sum_mon:
   assumes "\<forall> a \<in> A. (f a :: int) \<le> g a"
   shows "(\<Sum> a \<in> A. f a) \<le> (\<Sum> a \<in> A. g a)"
   using assms
-  by (induction A rule: infinite_finite_induct, auto)
+  by (induction A rule: infinite_finite_induct, simp_all)
+
+lemma distrib:
+  fixes
+    A :: "'a set" and
+    f :: "'a \<Rightarrow> int" and
+    g :: "'a \<Rightarrow> int"
+  shows "(\<Sum> a \<in> A. (f::'a \<Rightarrow> int) a) + (\<Sum> a \<in> A. g a) = (\<Sum> a \<in> A. (f a) + (g a))"
+  using sum.distrib
+  by metis
+
+lemma distrib_ereal:
+  fixes
+    A :: "'a set" and
+    f :: "'a \<Rightarrow> int" and
+    g :: "'a \<Rightarrow> int"
+  shows "ereal (real_of_int ((\<Sum> a \<in> A. (f::'a \<Rightarrow> int) a) + (\<Sum> a \<in> A. g a)))
+          = ereal (real_of_int ((\<Sum> a \<in> A. (f a) + (g a))))"
+  using distrib[of f]
+  by simp
+
+lemma uneq_ereal:
+  fixes
+    x :: int and
+    y :: int
+  assumes "x \<le> y"
+  shows "ereal (real_of_int x) \<le> ereal (real_of_int y)"
+  using assms
+  by simp
 
 subsection \<open>Swap Distance\<close>
 
 fun neq_ord :: "'a Preference_Relation \<Rightarrow> 'a Preference_Relation \<Rightarrow> 'a \<Rightarrow> 'a \<Rightarrow> bool" where
-  "neq_ord x y u v = (u \<preceq>\<^sub>x v \<and> v \<preceq>\<^sub>x u)"
+  "neq_ord r s a b = ((a \<preceq>\<^sub>r b \<and> b \<preceq>\<^sub>s a) \<or> (b \<preceq>\<^sub>r a \<and> a \<preceq>\<^sub>s b))"
 
 fun pairwise_disagreements :: "'a set \<Rightarrow> 'a Preference_Relation \<Rightarrow> 'a Preference_Relation \<Rightarrow>
                                       ('a \<times> 'a) set" where
-  "pairwise_disagreements A x y = {(u, v) \<in> A \<times> A. u \<noteq> v \<and> neq_ord x y u v}"
+  "pairwise_disagreements A r s = {(a, b) \<in> A \<times> A. a \<noteq> b \<and> neq_ord r s a b}"
 
 fun pairwise_disagreements' :: "'a set \<Rightarrow> 'a Preference_Relation \<Rightarrow> 'a Preference_Relation \<Rightarrow>
                                       ('a \<times> 'a) set" where
-  "pairwise_disagreements' A x y = Set.filter (\<lambda> (u, v). u \<noteq> v \<and> neq_ord x y u v) (A \<times> A)"
+  "pairwise_disagreements' A r s = Set.filter (\<lambda> (a, b). a \<noteq> b \<and> neq_ord r s a b) (A \<times> A)"
 
 lemma set_eq_filter:
   fixes
@@ -94,12 +114,12 @@ lemma pairwise_disagreements_eq[code]: "pairwise_disagreements = pairwise_disagr
   by fastforce
 
 fun swap :: "'a Vote Distance" where
-  "swap (A, x) (A', y) =
+  "swap (A, r) (A', r') =
     (if A = A'
-    then card (pairwise_disagreements A x y)
+    then card (pairwise_disagreements A r r')
     else \<infinity>)"
 
-lemma swap_case_inf:
+lemma swap_case_infinity:
   fixes
     x :: "'a Vote" and
     y :: "'a Vote"
@@ -143,32 +163,16 @@ lemma spearman_case_fin:
   using assms
   by (induction rule: spearman.induct, simp)
 
-lemma distrib:
-  fixes
-    A :: "'a set" and
-    f :: "'a \<Rightarrow> int" and
-    g :: "'a \<Rightarrow> int"
-  shows "(\<Sum> a \<in> A. (f::'a \<Rightarrow> int) a) + (\<Sum> a \<in> A. g a) = (\<Sum> a \<in> A. (f a) + (g a))"
-  using sum.distrib
-  by metis
+section \<open>Properties\<close>
 
-lemma distrib_ereal:
-  fixes
-    A :: "'a set" and
-    f :: "'a \<Rightarrow> int" and
-    g :: "'a \<Rightarrow> int"
-  shows "ereal (real_of_int ((\<Sum> a \<in> A. (f::'a \<Rightarrow> int) a) + (\<Sum> a \<in> A. g a)))
-          = ereal (real_of_int ((\<Sum> a \<in> A. (f a) + (g a))))"
-  using distrib[of f]
-  by simp
+definition distance_anonymity :: "'a Election Distance \<Rightarrow> bool" where
+  "distance_anonymity d \<equiv>
+    \<forall> A A' pi p p'.
+      (\<forall> n. (pi n) permutes {..< n}) \<longrightarrow>
+        d (A, p) (A', p') =
+          d (A, permute_list (pi (length p)) p) (A', permute_list (pi (length p')) p')"
 
-lemma uneq_ereal:
-  fixes
-    x :: int and
-    y :: int
-  assumes "x \<le> y"
-  shows "ereal (real_of_int x) \<le> ereal (real_of_int y)"
-  using assms
-  by simp
+definition standard :: "'a Election Distance \<Rightarrow> bool" where
+ "standard d \<equiv> \<forall> A A' p p'. length p \<noteq> length p' \<or> A \<noteq> A' \<longrightarrow> d (A, p) (A', p') = \<infinity>"
 
 end
